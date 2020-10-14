35b29c1b5207c840c2787a0112274ab2a0aaa049
bowcai <caibowen.cbw@outlook.com>
2020-10-14 17:08:04 +0800

    Upload Java version of Ex07

--- Ex0710_ExpressionTree/ExpressionTree.cpp
+++ Ex0710_ExpressionTree/ExpressionTree.cpp
@@ -4,28 +4,32 @@
 #include <queue>
 #include <string>
 #include <unordered_map>
+#include <memory>
 using namespace std;
 
-typedef char ElementType;
-
-typedef struct TreeNode* PtrToNode;
-typedef PtrToNode Tree;
-
-struct TreeNode
+template <typename T>
+class TreeNode
 {
-	ElementType data;
-	Tree Left;
-	Tree Right;
+public:
+	T data;
+	shared_ptr<TreeNode<T> > Left;
+	shared_ptr<TreeNode<T> > Right;
 	int Height;
+
+	// Create a new tree
+	TreeNode(T);
+
+	// Combine two trees with a new root node
+	TreeNode(T, const shared_ptr<TreeNode<T> >&
+		, const shared_ptr<TreeNode<T> >&);
+
+	void show(void);
 };
 
-Tree CreateTree(ElementType x);
-void DelTree(Tree T);
-Tree CombineTree(ElementType x, const Tree L, const Tree R);
 string ToPostfix(string S);
-int ComputePostfix(string postfix, const unordered_map<char, int>& dict);
-Tree PostfixToTree(string postfix);
-void PrintTree(const Tree T);
+int ComputePostfix(string postfix
+	, const unordered_map<char, int>& dict);
+shared_ptr<TreeNode<char> > PostfixToTree(string postfix);
 
 int main(void)
 {
@@ -48,44 +52,113 @@ int main(void)
 	cout << endl << "Output:" << endl;
 	string postfix = ToPostfix(infix);
 	cout << postfix << endl;
-	Tree T = PostfixToTree(postfix);
-	PrintTree(T);
+	shared_ptr<TreeNode<char> > T = PostfixToTree(postfix);
+	T->show();
 	cout << ComputePostfix(postfix, variables) << endl;
-
-	DelTree(T);
 
 	return 0;
 }
 
-Tree CreateTree(ElementType x)
+template <typename T>
+TreeNode<T>::TreeNode(T data) : data(data), Left(nullptr)
+, Right(nullptr), Height(0)
 {
-	Tree T = new TreeNode;
-	T->data = x;
-	T->Left = nullptr;
-	T->Right = nullptr;
-	T->Height = 0;
-	return T;
 }
 
-void DelTree(Tree T)
+template <typename T>
+TreeNode<T>::TreeNode(T data, const shared_ptr<TreeNode<T> >& Left
+	, const shared_ptr<TreeNode<T> >& Right)
+	: data(data), Left(Left), Right(Right)
 {
-	if (T == nullptr)
+	int leftHeight = Left ? Left->Height : -1;
+	int rightHeight = Right ? Right->Height : -1;
+	Height = leftHeight > rightHeight
+		? leftHeight + 1 : rightHeight + 1;
+}
+
+template <typename T>
+void TreeNode<T>::show(void)
+{
+	struct NodeDepth
 	{
-		return;
+		shared_ptr<TreeNode<T> > node;
+		int layer;
+
+		// The coordinate of node in output
+		int coordinate;
+	};
+	queue<NodeDepth> nodes;
+	vector<string> outs;
+
+	// Get the sequence of nodes
+	nodes.push({ shared_ptr<TreeNode<T>>(this), Height, (1 << Height) - 1 });
+
+	string s1 = "";
+	string s2 = "";
+	int coord1 = 0;
+	int coord2 = 0;
+	int last_layer = Height;
+
+	while (nodes.size() > 0)
+	{
+		NodeDepth current = nodes.front();
+		nodes.pop();
+
+		// Put last layer into output strings
+		if (last_layer != current.layer)
+		{
+			outs.push_back(s1);
+			outs.push_back(s2);
+			s1 = "";
+			s2 = "";
+			coord1 = 0;
+			coord2 = 0;
+			last_layer = current.layer;
+		}
+
+		// Deal with current node
+		for (int i = 0; i < current.coordinate - coord1; i++)
+		{
+			s1 += ' ';
+		}
+		s1 += current.node->data;
+		coord1 = current.coordinate + 1;
+
+		if (current.node->Left)
+		{
+			nodes.push({ current.node->Left, current.layer - 1
+				, current.coordinate - (1 << (current.layer - 1)) });
+
+			for (int i = 0; i < current.coordinate - 1 - coord2; i++)
+			{
+				s2 += ' ';
+			}
+			s2 += '/';
+			coord2 = current.coordinate;
+		}
+
+		if (current.node->Right)
+		{
+			nodes.push({ current.node->Right, current.layer - 1
+				, current.coordinate + (1 << (current.layer - 1)) });
+
+			for (int i = 0; i < current.coordinate + 1 - coord2; i++)
+			{
+				s2 += ' ';
+			}
+			s2 += '\\';
+			coord2 = current.coordinate + 2;
+		}
 	}
-	DelTree(T->Left);
-	DelTree(T->Right);
-	delete T;
+
+	outs.push_back(s1);
+
+	for (auto& s : outs)
+	{
+		cout << s << endl;
+	}
 }
 
-Tree CombineTree(ElementType x, const Tree L, const Tree R)
-{
-	Tree T = CreateTree(x);
-	T->Left = L;
-	T->Right = R;
-	T->Height = L->Height > R->Height ? L->Height + 1 : R->Height + 1;
-	return T;
-}
 
 // Convert the infix expression into postfix expression
 string ToPostfix(string S)
@@ -134,6 +207,11 @@ string ToPostfix(string S)
 			}
 			oper.push(c);
 		}
+		else
+		{
+			throw "The infix expression is illegal.";
+		}
+
 	}
 
 	while (oper.size() > 0)
@@ -148,7 +226,8 @@ string ToPostfix(string S)
 	return postfix;
 }
 
-int ComputePostfix(string postfix, const unordered_map<char, int>& dict)
+int ComputePostfix(string postfix
+	, const unordered_map<char, int>& dict)
 {
 	stack<int> numbers;
 	for (char& c : postfix)
@@ -162,8 +241,7 @@ int ComputePostfix(string postfix, const unordered_map<char, int>& dict)
 		{
 			if (numbers.size() < 2)
 			{
-				cout << "The postfix is illegal." << endl;
-				return 0;
+				throw "The postfix expression is illegal.";
 			}
 
 			int a = numbers.top();
@@ -189,133 +267,55 @@ int ComputePostfix(string postfix, const unordered_map<char, int>& dict)
 				break;
 			}
 		}
+		else
+		{
+			throw "The postfix expression is illegal.";
+		}
 	}
-	
+
 	if (numbers.size() != 1)
 	{
-		cout << "The postfix is illegal." << endl;
-		return 0;
+		throw "The postfix expression is illegal.";
 	}
 
 	return numbers.top();
 }
 
-Tree PostfixToTree(string postfix)
+shared_ptr<TreeNode<char> > PostfixToTree(string postfix)
 {
-	stack<Tree> nodes;
+	stack<shared_ptr<TreeNode<char> > > nodes;
 	for (char& c : postfix)
 	{
 		if ((c >= 'a' && c <= 'z')
 			|| (c >= 'A' && c <= 'Z'))
 		{
-			Tree temp = CreateTree(c);
-			nodes.push(temp);
+			nodes.push(shared_ptr<TreeNode<char> >(
+				new TreeNode<char>(c)));
 		}
 		else if (c == '+' || c == '-' || c == '*' || c == '/')
 		{
 			if (nodes.size() < 2)
 			{
-				cout << "Postfix is illegal and cannot convert to tree."
-					<< endl;
-				return nullptr;
+				throw "Postfix is illegal and cannot convert to tree.";
 			}
-			Tree right = nodes.top();
+			shared_ptr<TreeNode<char> > right = nodes.top();
 			nodes.pop();
-			Tree left = nodes.top();
+			shared_ptr<TreeNode<char> > left = nodes.top();
 			nodes.pop();
 
-			nodes.push(CombineTree(c, left, right));
+			nodes.push(shared_ptr<TreeNode<char> >(
+				new TreeNode<char>(c, left, right)));
+		}
+		else
+		{
+			throw "Postfix is illegal and cannot convert to tree.";
 		}
 	}
 
 	if (nodes.size() != 1)
 	{
-		cout << "Postfix is illegal and cannot convert to tree."
-			<< endl;
-		return nullptr;
+		throw "Postfix is illegal and cannot convert to tree.";
 	}
 
 	return nodes.top();
-}
-
-void PrintTree(const Tree T)
-{
-	struct NodeDepth
-	{
-		Tree node;
-		int layer;
-
-		// The coordinate of node in output
-		int coordinate;
-	};
-	queue<NodeDepth> nodes;
-	vector<string> outs;
-
-	// Get the sequence of nodes
-	nodes.push({ T, T->Height, (1 << T->Height) - 1 });
-
-	string s1 = "";
-	string s2 = "";
-	int coord1 = 0;
-	int coord2 = 0;
-	int last_layer = T->Height;
-
-	while (nodes.size() > 0)
-	{
-		struct NodeDepth current = nodes.front();
-		nodes.pop();
-
-		// Put last layer into output strings
-		if (last_layer != current.layer)
-		{
-			outs.push_back(s1);
-			outs.push_back(s2);
-			s1 = "";
-			s2 = "";
-			coord1 = 0;
-			coord2 = 0;
-			last_layer = current.layer;
-		}
-
-		// Deal with current node
-		for (int i = 0; i < current.coordinate - coord1; i++)
-		{
-			s1 += ' ';
-		}
-		s1 += current.node->data;
-		coord1 = current.coordinate + 1;
-		
-		if (current.node->Left != nullptr)
-		{
-			nodes.push({ current.node->Left, current.layer - 1
-				, current.coordinate - (1 << (current.layer - 1)) });
-
-			for (int i = 0; i < current.coordinate - 1 - coord2; i++)
-			{
-				s2 += ' ';
-			}
-			s2 += '/';
-			coord2 = current.coordinate;
-		}
-		
-		if (current.node->Right != nullptr)
-		{
-			nodes.push({ current.node->Right, current.layer - 1
-				, current.coordinate + (1 << (current.layer - 1)) });
-
-			for (int i = 0; i < current.coordinate + 1 - coord2; i++)
-			{
-				s2 += ' ';
-			}
-			s2 += '\\';
-			coord2 = current.coordinate + 2;
-		}
-	}
-
-	outs.push_back(s1);
-
-	for (auto& s : outs)
-	{
-		cout << s << endl;
-	}
 }
